2edc92cf8b6b95543d69235db8780f3afa132f1a
Amir Bouasker <bouasker@hotmail.com>
2024-01-28 06:22:56 -0500

    change information package

--- packages/regression_model/setup.py
+++ packages/regression_model/setup.py
@@ -9,20 +9,20 @@ from setuptools import find_packages, setup
 
 
 # Package meta-data.
-NAME = 'regression_model'
+NAME = "regression_model"
 # DESCRIPTION = 'Train and deploy regression model.'
 # URL = 'your github project'
 # EMAIL = 'your_email@email.com'
 # AUTHOR = 'Your name'
-DESCRIPTION = 'Former et deployer un modele de regression.'
-URL = 'https://github.com/cegep2020/cours-A61.git'
-EMAIL = 'aamami@csfoy.ca'
-AUTHOR = 'Cegep AA'
-REQUIRES_PYTHON = '>=3.6.0'
+DESCRIPTION = "Former et deployer un modele de regression."
+URL = "https://github.com/AmirBouasker/TP1_2196901.git"
+EMAIL = "2196901@csfoy.ca"
+AUTHOR = "Amir Bouasker"
+REQUIRES_PYTHON = ">=3.6.0"
 
 
 # What packages are required for this module to be executed?
-def list_reqs(fname='requirements.txt'):
+def list_reqs(fname="requirements.txt"):
     with open(fname) as fd:
         return fd.read().splitlines()
 
@@ -38,8 +38,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
@@ -48,38 +48,38 @@ except FileNotFoundError:
 ROOT_DIR = Path(__file__).resolve().parent
 PACKAGE_DIR = ROOT_DIR / NAME
 about = {}
-with open(PACKAGE_DIR / 'VERSION') as f:
+with open(PACKAGE_DIR / "VERSION") as f:
     _version = f.read().strip()
-    about['__version__'] = _version
+    about["__version__"] = _version
 
 
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
-    package_data={'regression_model': ['VERSION']},
+    packages=find_packages(exclude=("tests",)),
+    package_data={"regression_model": ["VERSION"]},
     install_requires=list_reqs(),
     extras_require={},
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
 )
