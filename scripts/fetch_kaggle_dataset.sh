0946461969273150459eaf4f466cc07d0ede1dc8
Amir Bouasker <bouasker@hotmail.com>
2024-02-04 12:13:29 -0500

    uzip files

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
-unzip -d . packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
+unzip -d /home/circleci/project/packages/regression_model/regression_model/datasets/ packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
